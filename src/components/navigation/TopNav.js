0a4d83d3a219a3efe2cc3ed73aa07401dfd22285
Nathan Snow <nathan.snow@cqumail.com>
2021-11-27 15:45:14 +1000

    Refactor to include react bootstrap

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -1,48 +1,40 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 import './TopNavStyle.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
+const theme = "dark";
 
-//Make active nav links dynamic 
 const IsActive = (endpoint) => {
   const currentPath = useLocation().pathname;
-  const inactiveLink = "nav-link";
-  const activeLink = inactiveLink + " active";
-  return currentPath === endpoint ? activeLink : inactiveLink;
+  return currentPath === endpoint ? "active" : null;
 }
 
+const endpoints = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Contact", path: "/contact" }
+]
 
 const TopNav = () => {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky">
-        <div className="container">
-          <a className="navbar-brand auto-display" href="/">
-            My Portfolio
-          </a>
-          {/* --- Navbar menu toggler button --- */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          {/* --- Collapsible nav menu --- */}
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            {/* --- Navigation Links --- */}
-            <div className="navbar-nav">
-              <Link className={IsActive("/")} to="/">
-                Home
-              </Link>
-              <Link className={IsActive("/about")} to="/about">
-                About
-              </Link>
-              <Link className={IsActive("/contact")} to="/contact">
-                Contact
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
+  return (
+    <Navbar bg={theme} variant={theme} expand="lg" fixed="top">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+          <Nav>
+            {endpoints.map((endpoint, index) => {
+              return (
+                <Nav.Link className={IsActive(endpoint.path)} href={endpoint.path} key={index}>
+                  {endpoint.name}
+                </Nav.Link>
+              );
+            })}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default TopNav;
