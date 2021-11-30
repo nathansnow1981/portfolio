c7095da03809a7b552731d1624a1f7eb8da32d97
Nathan Snow <nathan.snow@cqumail.com>
2021-11-30 21:18:28 +1000

    Implement API call and dynamic page fields

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { Col, Container, Card } from 'react-bootstrap';
 import WebsterConfig from '../../config/WebsterConfig';
 
+/**
+ * Visual settings
+ */
 const variant = "dark";
 const cardBorderVariant = "light";
 const cardTextVariant = "light";
@@ -9,46 +12,20 @@ const columnClassName = "mx-auto";
 const cardClassName = "m-3";
 const imagePosition = "top";
 const colXs = 12, colSm = 6, colMd = 6, colLg = 4;
-const projects = [
-  {
-    id: 1,
-    name: "Project 1",
-    desc: "Just a short description",
-    img: "https://cdn.pixabay.com/photo/2017/08/25/13/36/code-geek-2680204_960_720.png",
-  },
-  {
-    id: 2,
-    name: "Project 2",
-    desc: "Just a short description",
-    img: "https://cdn.pixabay.com/photo/2017/08/25/13/36/code-geek-2680204_960_720.png",
-  },
-  {
-    id: 3,
-    name: "Project 3",
-    desc: "Just a short description",
-    img: "https://cdn.pixabay.com/photo/2017/08/25/13/36/code-geek-2680204_960_720.png",
-  },
-  {
-    id: 4,
-    name: "Project 4",
-    desc: "Just a short description",
-    img: "https://cdn.pixabay.com/photo/2017/08/25/13/36/code-geek-2680204_960_720.png",
-  },
-];
 
+/**
+ * Queries the API for the current projects
+ * @returns Returns a list of project objects
+ */
+const currentProjectsList = () => {
 
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", WebsterConfig.projectsUrl, false);
+    xhr.send();
+    return JSON.parse(xhr.responseText)
 
-let xhr = new XMLHttpRequest()
-xhr.open("get", WebsterConfig.projectsUrl)
-xhr.send()
-
-xhr.onload = () => {
-    globalList(JSON.parse(xhr.response))
-};
-
-const globalList = (list) => {
-    return list
 }
+
 
 const Home = () => {
 
@@ -59,15 +36,15 @@ const Home = () => {
     return (
       <Container className="flex-container">
           {
-              projects.map((proj, id) => {
+              currentProjectsList().map((project, index) => {
                   return (
-                    <Col className={columnClassName} xs={colXs} sm={colSm} md={colMd} lg={colLg} key={id}>
+                    <Col className={columnClassName} xs={colXs} sm={colSm} md={colMd} lg={colLg} key={index}>
                       <Card border={cardBorderVariant} text={cardTextVariant} bg={variant} className={cardClassName}>
-                        <Card.Header>{proj.name}</Card.Header>
-                        <Card.Img variant={imagePosition} src={proj.img} />
+                        <Card.Header>{project.header}</Card.Header>
+                        <Card.Img variant={imagePosition} src={project.image} />
                         <Card.Body>
-                          <Card.Title>{proj.name}</Card.Title>
-                          <Card.Text>{proj.desc}</Card.Text>
+                          <Card.Title>{project.title}</Card.Title>
+                          <Card.Text>{project.description}</Card.Text>
                         </Card.Body>
                       </Card>
                     </Col>
